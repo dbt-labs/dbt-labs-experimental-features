5ecd1f1735da7a4d01aae165466aa9785135962e
Claire Carroll <carroll.claire.e@gmail.com>
2020-08-03 15:06:47 -0400

    Copy option 1

---
+++ lambda-views/models/option_2/page_views__lambda_historical.sql
@@ -0,0 +1,35 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'page_view_id'
+    )
+}}
+
+with events as (
+
+    select * from {{ source('snowplow','event') }}
+    {% if is_incremental() %}
+    where collector_tstamp >= (select max(collector_tstamp) from {{ this }})
+    {% endif %}
+
+),
+
+page_views as (
+
+    select
+        domain_sessionid as session_id,
+        domain_userid as anonymous_user_id,
+        web_page_context.value:data.id::varchar as page_view_id,
+        page_url,
+        count(*) * 10 as approx_time_on_page,
+        min(derived_tstamp) as page_view_start,
+        max(collector_tstamp) as collector_tstamp
+
+    from events,
+    lateral flatten (input => parse_json(contexts):data) web_page_context
+
+    group by 1,2,3,4
+
+)
+
+select * from page_views
