3c272ad715a952bb781d977dc36f831af512c610
Dave Connors <dave.connors@fishtownanalytics.com>
2021-03-25 11:56:54 -0500

    all macros to example

--- biz-hours/models/fct_support_tickets.sql
+++ biz-hours/models/fct_support_tickets.sql
@@ -8,8 +8,10 @@ final as (
     
     select 
         tickets.*,
+        {{ weekdays_between('conversation_created_at_business', 'first_response_at_business') }} as weekdays_to_first_response,
+        {{ non_business_hours_between('conversation_created_at_business', 'first_response_at_business') }} as non_working_hours,
+        {{ business_minutes_between('conversation_created_at_business', 'first_response_at_business') }} as business_minutes
         
-        {{ non_business_hours_between('conversation_created_at_business', 'first_response_at_business') }}
     
     from tickets   
     
