2e9f61af9e08df94d9d767447ab77476ca5951d3
Claire Carroll <20294432+clrcrl@users.noreply.github.com>
2021-04-19 11:53:08 -0400

    Remove _business

--- biz-hours/models/ticket_hours.sql
+++ biz-hours/models/ticket_hours.sql
@@ -18,8 +18,8 @@ ticket_hours as (
             when business_hours.is_business_hour
                 then datediff(
                     'minutes',
-                    greatest(business_hours.date_hour_start, tickets.conversation_created_at_business),
-                    least(business_hours.date_hour_end, tickets.first_response_at_business)
+                    greatest(business_hours.date_hour_start, tickets.conversation_created_at),
+                    least(business_hours.date_hour_end, tickets.first_response_at)
                 )
             else 0
         end as business_minutes_to_first_solve,
@@ -29,8 +29,8 @@ ticket_hours as (
             when business_hours.is_business_hour
                 then datediff(
                     'minutes',
-                    greatest(business_hours.date_hour_start, tickets.conversation_created_at_business),
-                    least(business_hours.date_hour_end, tickets.first_closed_at_business)
+                    greatest(business_hours.date_hour_start, tickets.conversation_created_at),
+                    least(business_hours.date_hour_end, tickets.first_closed_at)
                 )
             else 0
         end as business_minutes_to_first_close,
@@ -39,8 +39,8 @@ ticket_hours as (
             when business_hours.is_business_hour
                 then datediff(
                     'minutes',
-                    greatest(business_hours.date_hour_start, tickets.conversation_created_at_business),
-                    least(business_hours.date_hour_end, tickets.last_closed_at_business)
+                    greatest(business_hours.date_hour_start, tickets.conversation_created_at),
+                    least(business_hours.date_hour_end, tickets.last_closed_at)
                 )
             else 0
         end as business_minutes_to_last_close
@@ -49,9 +49,9 @@ ticket_hours as (
     from tickets
 
     left join business_hours
-        on date_trunc('hour', tickets.conversation_created_at_business) <= business_hours.date_hour_start
+        on date_trunc('hour', tickets.conversation_created_at) <= business_hours.date_hour_start
         -- in reality don't need to do this pre-processing
-        and date_trunc('hour', tickets.last_closed_at_business) >= business_hours.date_hour_start
+        and date_trunc('hour', tickets.last_closed_at) >= business_hours.date_hour_start
 
 )
 
