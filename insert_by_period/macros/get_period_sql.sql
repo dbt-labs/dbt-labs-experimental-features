259272c8ac12a05ce637e80a7971ccbb699d2d4f
Jonathan Gough <jonathanpgough@gmail.com>
2024-09-05 11:17:21 +0100

    Add support for Athena

--- insert_by_period/macros/get_period_sql.sql
+++ insert_by_period/macros/get_period_sql.sql
@@ -57,3 +57,22 @@
   ) target_cols
 
 {%- endmacro %}
+
+
+{% macro athena__get_period_sql(target_cols_csv, sql, timestamp_field, period, start_timestamp, stop_timestamp, offset) -%}
+
+  {%- set period_filter -%}
+    ({{timestamp_field}} >  cast('{{start_timestamp}}' as timestamp) + interval '{{offset}}' {{period}} and
+     {{timestamp_field}} <= cast('{{start_timestamp}}' as timestamp) + interval '{{offset}}' {{period}} + interval '1' {{period}}  and
+     {{timestamp_field}} <  cast('{{stop_timestamp}}' as timestamp))
+  {%- endset -%}
+
+  {%- set filtered_sql = sql | replace("__PERIOD_FILTER__", period_filter) -%}
+
+  select
+    {{target_cols_csv}}
+  from (
+    {{filtered_sql}}
+  ) target_cols
+
+{%- endmacro %}
